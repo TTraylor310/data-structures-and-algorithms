40a0d158b45f5f321cc15b68dd2a390cb0839328
Tim Traylor <ttraylor83@gmail.com>
2022-10-11 07:23:15 -0700

    editorconfig fix..ugh

---
+++ javascript/ temps/binary-serach-tree.js
@@ -0,0 +1,59 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// class KaryNode{
+//   constructor(value, k) {
+//     this.value = value;
+//     this.k = k;
+//     this.children = [];
+//   }
+// }
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    let newNode = new Node (value);
+    if(this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(node, newNode) {
+    if(newNode.value < node.value) {
+      if(node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+}
+
+
+let BST = new BinarySearchTree();
+BST.add(1);
+BST.add(3);
+BST.add(2);
+
+console.log(BST);
+
+
