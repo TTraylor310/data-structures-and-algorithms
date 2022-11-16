67d70ed2024cd7996640da1cc18912ec0550c5e0
Tim Traylor <ttraylor83@gmail.com>
2022-11-16 04:59:06 +0000

    done with trees

--- javascript/DataStructures&Algo/checkTest.js
+++ javascript/DataStructures&Algo/checkTest.js
@@ -7,3 +7,13 @@ const myStack = new Stack();
 const { Queue } = require('./linkedList-queue');
 const que = new Queue();
 
+const {BST} = require('./tree-basic');
+const tree = new BST();
+
+tree.insert(47);
+tree.insert(76); 
+tree.insert(21);
+
+console.log(tree);
+
+tree.traversal(tree);
--- javascript/DataStructures&Algo/tree-basic.js
+++ javascript/DataStructures&Algo/tree-basic.js
@@ -1,32 +1,73 @@
- class Node {
+class Node {
   constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+    this.value = value
+    this.left = null
+    this.right = null
   }
- }
+}
 
 class BST {
   constructor() {
-    this.root = null;
+    this.root = null
+  }
+
+  traversal(tree) {
+    const recTraverse = node => {
+      console.log(node.value);
+      if (node.left) recTraverse(node.left)
+      if (node.right) recTraverse(node.right)
+    }
+    recTraverse(tree.root)
   }
 
   insert(value) {
-    const newNode = new Node(value);
-    if (this.root === null){
-      this.root = newNode;
-      return this;
+    const newNode = new Node(value)
+    if (this.root === null) {
+      this.root = newNode
+      return this
     }
-    let current = this.root;
-    while(true){
-      if (newNode.value === current.value) return undefined;
-
-
-
-
-
-
+    let current = this.root
+    while (true) {
+      if (newNode.value === current.value) return undefined
+      if (newNode.value < current.value) {
+        if (current.left === null) {
+          current.left = newNode
+          return this
+        }
+        current = current.left
+      } else {
+        if (current.right === null) {
+          current.right = newNode
+          return this
+        }
+        current = current.right
+      }
     }
   }
 
+  contains(value) {
+    if (this.root === null) return false
+    let current = this.root
+    while (current) {
+      if (value < current.value) {
+        current = current.left
+      } else if (value > current.value) {
+        current = current.right
+      } else {
+        return true
+      }
+    }
+    return false
+  }
+
+  minValueNode(currentNode) {
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode;
+  }
+
+
 }
+
+module.exports = { BST }
