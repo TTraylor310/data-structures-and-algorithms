2d1160193d7b434beb22a5b5c7bf07be39223982
Tim Traylor <ttraylor83@gmail.com>
2022-11-21 17:01:18 +0000

    sorting fun with merge

--- javascript/DataStructures&Algo/sorting-basic.js
+++ javascript/DataStructures&Algo/sorting-basic.js
@@ -10,8 +10,8 @@ function bubbleSort(array) {
   }
   return array;
 }
-
 // console.log(bubbleSort([4,2,6,5,1,3]));
+
 
 function selectionSort(array) {
   let min;
@@ -28,8 +28,8 @@ function selectionSort(array) {
   }
   return array;
 }
-
 // console.log(selectionSort([4, 2, 6, 5, 1, 3]));
+
 
 function insertionSort(array) {
   let temp;
@@ -42,5 +42,43 @@ function insertionSort(array) {
   }
   return array;
 }
-
 // console.log(insertionSort([2, 1, 4, 3, 5, 6]));
+// O(n^2) typically
+
+
+function merge (array1, array2){
+  let combined = [];
+  let i = 0;
+  let j = 0;
+  while (i < array1.length && j < array2.length) {
+    if (array1[i] < array2[j]){
+      combined.push(array1[i])
+      i++
+    } else {
+      combined.push(array2[j])
+      j++
+    }
+  }
+  while (i < array1.length){
+    combined.push(array1[i])
+    i++
+  }
+  while (j < array2.length){
+    combined.push(array2[j])
+    j++
+  }
+  return combined;
+}
+// console.log(merge([1,3,7,8], [2,4,5,6]));
+
+
+function mergeSort(array) {
+  if (array.length === 1) return array
+  let mid = Math.floor(array.length/2)
+  let left = array.slice(0, mid)
+  let right = array.slice(mid)
+  return merge(mergeSort(left), mergeSort(right))
+}
+// console.log(mergeSort([1,3,7,8,2,4,5,6]));
+// O(n log n)
+
